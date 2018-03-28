dc205d79f3b7643473fd3ac15e3284b5fd3b3985
MGodgildieva <mariagodg@gmail.com>
2018-03-28 14:06:14 +0300

    new ticket

--- src/main/java/telran/tickets/dao/PdfCreator.java
+++ src/main/java/telran/tickets/dao/PdfCreator.java
@@ -1,9 +1,6 @@
 package telran.tickets.dao;
 
-import java.awt.Color;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,9 +19,6 @@ import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.kernel.pdf.extgstate.PdfExtGState;
 import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
 import com.itextpdf.layout.Canvas;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.border.Border;
-import com.itextpdf.layout.border.SolidBorder;
 import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.property.TextAlignment;
@@ -40,40 +34,7 @@ public class PdfCreator {
 	public PdfCreator(EventSeat eventSeat) {
 		this.eventSeat = eventSeat;
 	}
-
-	public void createTicketInFolder() throws IOException {
-		PdfWriter writer = new PdfWriter("D:\\TicketDocs\\ticket_example.pdf");
-		PdfDocument pdf = new PdfDocument(writer);
-		Document document = new Document(pdf);
-		createPage(eventSeat, document);
-		document.close();
-	}
 	
-	public void createPage(EventSeat eventSeat, Document document) throws IOException {
-		PdfFont boldFont = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
-		Border border = new SolidBorder(1);
-		Image logo = new Image(ImageDataFactory.create("logoColoured.png"));
-		logo.setFixedPosition(370, 750);
-		BarcodeQRCode barcode = new BarcodeQRCode(eventSeat.getId().toString());
-		java.awt.Image barcodeImg = barcode.createAwtImage(Color.BLACK, Color.WHITE);
-		Image barcodeImage = new Image(ImageDataFactory.create(barcodeImg, null));
-		barcodeImage.setFixedPosition(370, 550);
-		barcodeImage.setWidth(150);
-		barcodeImage.setHeight(150);
-		Paragraph title = new Paragraph("Ticket Service");
-		title.setFont(boldFont);
-		title.setFontSize(24);
-		title.setUnderline();
-		title.setHeight(60);
-		document.add(title);
-		document.add(logo);
-		document.add(new Paragraph(eventSeat.getEvent().getTitle() + "\n" + eventSeat.getEvent().getDate().toString() + " & " + eventSeat.getEvent().getTime()).setWidth(200).setBorder(border).setFont(boldFont).setKeepTogether(true));
-		document.add(new Paragraph(eventSeat.getEvent().getHall().getHallName()).setKeepWithNext(true));
-		document.add(new Paragraph(eventSeat.getEvent().getHall().getCity() + ", " + eventSeat.getEvent().getHall().getStreet() + ", "+eventSeat.getEvent().getHall().getHouse()));
-		document.add(new Paragraph("Seat: Row: " + eventSeat.getSeat().getRealRow() + ", Place: " + eventSeat.getSeat().getRealPlace()).setKeepWithNext(true));
-		document.add(new Paragraph(eventSeat.getPrice()));
-		document.add(barcodeImage);
-	}
 	public void createTicketInRectangle(EventSeat eventSeat, PdfDocument pdf) throws IOException {
 		float width = pdf.getDefaultPageSize().getWidth();
 	    float height = pdf.getDefaultPageSize().getHeight();
@@ -135,29 +96,4 @@ public class PdfCreator {
 		baos.close();
 		return pdfToBytes;
 	}
-	
-	public Document createTicket(ByteArrayOutputStream baos) throws IOException {
-		PdfDocument pdfDoc = new PdfDocument(new PdfWriter(baos));
-		Document document = new Document(pdfDoc);
-		document.setMargins(50, 50, 50, 50);
-		PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA);
-		document.setFont(font);
-		document.setFontSize(18);
-		return document;
-	}
-	public byte[] saveDocToByteArray(Document document, ByteArrayOutputStream baos) throws IOException {
-		document.close();
-		byte [] pdfToBytes = baos.toByteArray();
-		baos.close();
-		return pdfToBytes;
-	}
-	
-	public void getPdfFromByteArray(byte[] arr) throws IOException {
-		File someFile = new File("D:\\TicketDocs\\ticket.pdf");
-        FileOutputStream fos = new FileOutputStream(someFile);
-        fos.write(arr);
-        fos.flush();
-        fos.close();
-	}
-
 }
